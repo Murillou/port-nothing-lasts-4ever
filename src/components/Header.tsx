387579c93252519de7e3b2f513011b65aaee6a1a
Murillou <Murillovinicius13@hotmail.com>
2024-05-15 18:48:37 -0300

    [feat] responsividade do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>
+      <h1 className={styles.nameLogo}>
         {' '}
         NOTHING <span className={styles.lasts}>LASTS</span> 4EVER
       </h1>
